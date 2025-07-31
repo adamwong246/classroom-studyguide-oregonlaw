6fefe9497cdc9840b8b41e4e7639175d3a728c68
adam wong <adamwong246@gmail.com>
2025-07-30 21:50:33 -0700

    wtf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/classroom-studyguide-oregonlaw/'
-})
+  base: "/classroom-studyguide-oregonlaw/",
+  build: {
+    outDir: "docs", // This line specifies the output directory
+  },
+});
